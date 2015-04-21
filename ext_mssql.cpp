e22cc7e196f90b821271755c6199b8eec596a732
Andy Robinson <arobinson@sales-i.com>
2015-04-21 16:49:59 +0100

    Adding mssql_bind skeleton

--- ext_mssql.cpp
+++ ext_mssql.cpp
@@ -3,14 +3,24 @@
 
 namespace HPHP {
 
-static Variant HHVM_FUNCTION(mssql_connect, const String& server_name, const String& username, const String& password, bool new_link /* = 0 */) {
+/** http://docs.hhvm.com/manual/en/function.mssql-bind.php **/
+static bool HHVM_FUNCTION(mssql_bind, const Resource& stmt, const String& param_name, const Variant& var, int64_t type,
+                      bool is_output = false, bool is_null = false, int maxlen = -1) {
     // TODO - Implement
-    return Variant(0);
+    return false;
 }
 
-static bool HHVM_FUNCTION(mssql_close,  const Variant& link_identifier) {
+/** http://docs.hhvm.com/manual/en/function.mssql-close.php **/
+static bool HHVM_FUNCTION(mssql_close,  const Resource& link_identifier) {
     // TODO - Implement
     return 0;
+}
+
+/** http://docs.hhvm.com/manual/en/function.mssql-connect.php **/
+static Variant HHVM_FUNCTION(mssql_connect, const String& server_name, const String& username, const String& password,
+                        bool new_link /* = 0 */) {
+    // TODO - Implement
+    return Variant(0);
 }
 
 static class mssqlExtension : public Extension {
@@ -19,8 +29,9 @@ public:
 
     virtual void moduleInit() {
 
-        HHVM_FE(mssql_connect);
+        HHVM_FE(mssql_bind);
         HHVM_FE(mssql_close);
+        HHVM_FE(mssql_connect);
 
         loadSystemlib();
     }
--- ext_mssql.php
+++ ext_mssql.php
@@ -1,8 +1,11 @@
 <?hh
 
+
+<<_Native>>
+function mssql_bind(resource $stmt, string $param_name, mixed &$var, int $type, bool $is_output = false, bool $is_null = false, int $maxlen = -1 ): bool;
+
 <<__Native>>
 function mssql_connect(string $servername, string $username, string $password, bool $new_link = false): mixed;
 
 <<_Native>>
 function mssql_close(resource $link_identifier): bool;
-
