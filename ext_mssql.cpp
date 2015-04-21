0dfebb73a93dd5d77ef417a5ec9859fae97df65f
Andy Robinson <arobinson@sales-i.com>
2015-04-21 16:25:44 +0100

    Removing unecessary namespace

--- ext_mssql.cpp
+++ ext_mssql.cpp
@@ -1,10 +1,7 @@
 
 #include "hphp/runtime/base/base-includes.h"
 
-
 namespace HPHP {
-
-namespace { // Anonymous namespace
 
 static Variant HHVM_FUNCTION(mssql_connect, const String& server_name, const String& username, const String& password, bool new_link /* = 0 */) {
     // TODO - Implement
@@ -16,9 +13,6 @@ static bool HHVM_FUNCTION(mssql_close,  const Variant& link_identifier) {
     return 0;
 }
 
-
-
-
 static class mssqlExtension : public Extension {
 public:
     mssqlExtension() : Extension("mssql") {}
@@ -28,13 +22,10 @@ public:
         HHVM_FE(mssql_connect);
         HHVM_FE(mssql_close);
 
-		loadSystemlib();
+        loadSystemlib();
     }
 } s_mssql_extension;
 
-
 HHVM_GET_MODULE(mssql)
-
-} // Anonymous namespace
 
 }
